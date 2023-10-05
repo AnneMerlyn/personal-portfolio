4165bb5e005d4a3c566f7c4e4d591360dcf3813b
Anne Merlyn Martinez <martinezannemerlyn@gmail.com>
2023-10-05 18:37:18 +0200

    fix layout issue on md display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,15 +71,15 @@ export default function Home() {
                             </li>
                         </ul>
                     </nav>
-                    <div className="flex items-center justify-center max-w-3xl mx-auto gap-9">
-                        <div className="flex-none shadow-md bg-gradient-to-b from-pink-300 to-lavender-300 rounded-full w-40 h-40 relative overflow-hidden md:w-64 md:h-64">
+                    <div className="flex flex-col md:flex-row items-center justify-center max-w-3xl mx-auto gap-6 mt-8 md:gap-9">
+                        <div className="shadow-md bg-gradient-to-b from-pink-300 to-lavender-300 rounded-full w-40 h-40 relative overflow-hidden md:w-64 md:h-64 md:flex-none">
                             <Image
                                 src={devan}
                                 layout="fill"
                                 objectFit="cover"
                             />
                         </div>
-                        <div className="flex-grow text-left space-y-5">
+                        <div className="flex-grow  text-center md:text-left space-y-5 mt-6 md:mt-0">
                             <p className="text-xl font-semi-bold bg-pink-200 dark:bg-pink-400 inline-block rounded-sm px-1 py-0.5 dark:text-gray-200">
                                 Hey there, I'm
                             </p>
@@ -108,7 +108,7 @@ export default function Home() {
                             </p>
                         </div>
                     </div>
-                    <div className="text-2xl flex items-center justify-center gap-2 py-3 text-gray-700 dark:text-gray-400">
+                    <div className="text-xl md:text-2xl flex flex-wrap items-center justify-center gap-2 py-3 text-gray-700 dark:text-gray-400">
                         <a
                             href="https://www.linkedin.com/in/anne-merlyn-martinez/"
                             target="_blank"
